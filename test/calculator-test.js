1637358d1a25e884a3b004412b6fa762384fe139
Pedro <pedroaraujo_41@hotmail.com>
2020-12-04 17:41:41 +0000

    teste

--- test/calculator-test.js
+++ test/calculator-test.js
@@ -1,12 +1,12 @@
 const { assert } = require('chai');
 const { double } = require('../calculator');
 
-const value = 3;
+const value = 2;
 
 describe('Calculator', () => {
   it(`Should return the double of ${value}`, () => {
     const result = double(value);
     assert.isNumber(result, 'Not a number!');
-    assert.equal(result, 6, 'The result does not match!');
+    assert.equal(result, 4, 'The result does not match!');
   });
 });
